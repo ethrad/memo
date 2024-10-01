2cea2738c638fff8af0afc62b54b8f951d9e84e7
ethrad <asdf123f4f4@gmail.com>
2024-10-01 22:18:10 +0900

    객체 중복 생성 문제 해결

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -21,7 +21,6 @@ public class MemoRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-
     public Memo save(Memo memo) {
         // DB 저장
         KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환받기 위한 객체
